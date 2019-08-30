ab7126aa5fd9d9cd2812121e1cd7574a056e4290
ShettySUJITH <sujith.x@314e.com>
2019-08-30 12:21:26 +0530

    Update run.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-
-
 jwt=JWT(app,authenticate, identity) #/auth
 
 api.add_resource(Store, '/store/<string:name>')
