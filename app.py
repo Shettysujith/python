27e7f9752dbceb5bc6c22339dbbfca7b499d1359
ShettySUJITH <sujith.x@314e.com>
2019-08-30 12:09:12 +0530

    Update run.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Itemlist, '/items')
 api.add_resource(UserRegister, '/register')
 
-# if __name__=='__main__':
-    # from db import db
-    # db.init_app(app)
-    # app.run(port=5000, debug=True)
+if __name__=='__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
